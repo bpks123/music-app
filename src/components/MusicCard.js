fd27ba8f070f6ccae8c09d5533f1df6e5096e1e1
Bipin Kumar <139612124+bpks123@users.noreply.github.com>
2024-02-28 22:02:11 +0530

    updated

---
+++ src/components/MusicCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MusicCard() {
+  return (
+    <div>
+      Music card
+    </div>
+  )
+}
---
+++ src/pages/Registar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Registar() {
+  return (
+    <div>
+      Register
+    </div>
+  )
+}
