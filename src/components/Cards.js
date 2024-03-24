307e532a03eacb855ae8705f9fd8331f95846ce4
Bipin Kumar <139612124+bpks123@users.noreply.github.com>
2024-03-24 14:55:56 +0530

    updarte

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,37 +2,49 @@ import React, { useState } from 'react'
 import './Cards.css'
 import noNameImage from '../photos/no-name.jpg'
 import { useStateProvider } from '../Utils/StateProvider'
+import { Link,useNavigate } from 'react-router-dom'
+
 export default function Cards({posts}) {
+  
+    const navigate=useNavigate()
     const [{token},dispatch]=useStateProvider()
     const [getTitle,setTitle]=useState(posts.content.slice(0,50))
     const [isLike,setIsLike]=useState(false)
     const [getlikeCount,setLikeCount]=useState(posts.likeCount)
+    const [getCommentCount,setCommentCount]=useState(posts.commentCount)
     const [getIsComment,setIsComment]=useState(false)
     const [getDate,setDate]=useState('')
     const [getComment,setComment]=useState('')
     const [isCommented,setIsCommented]=useState(false)
     const [getCommentContent,setCommentContent]=useState('')
     const onClickLike=async(id)=>{
-      try{
-        let response=await fetch(`https://academics.newtonschool.co/api/v1/quora/like/${id}`,{
-          method:'POST',
-          headers:{
-          projectId:'l2uaz7omaxbe',
-          Authorization: `Bearer ${token}`
-        },
-        body:null
-        })
-        console.log(response)
-        if(response.status==201){
-            setIsLike(true)
-            setLikeCount(getlikeCount+1)
+      if(token){
+        try{
+          let response=await fetch(`https://academics.newtonschool.co/api/v1/quora/like/${id}`,{
+            method:'POST',
+            headers:{
+            projectId:'l2uaz7omaxbe',
+            Authorization: `Bearer ${token}`
+          },
+          body:null
+          })
+          console.log(response)
+          if(response.status==201){
+              setIsLike(true)
+              setLikeCount(getlikeCount+1)
+          }
+          if(response.status==400){
+            alert("You have already liked the post!")
+          }
+        }catch(error){
+          alert(error.message)
         }
-        if(response.status==400){
-          alert("You have already liked the post!")
-        }
-      }catch(error){
-        alert(error.message)
       }
+      else{
+        alert('Please Login to Like post!')
+        navigate('/login')
+      }
+      
     }
     async function onHandleClickComment(id){
       try{
@@ -45,31 +57,38 @@ export default function Cards({posts}) {
         body:null
         })
         console.log(response)
-        // if(response.status==201){
-        //     setIsLike(true)
-        //     setLikeCount(getlikeCount+1)
-        // }
-        // if(response.status==400){
-        //   alert("You have already liked the post!")
-        // }
+        if(response.status==200){
+          setIsCommented(true)
+          setCommentContent(getComment)
+          setCommentCount(getCommentCount+1)
+        }
+        if(response.status==400){
+          alert("Something went wrong. Try again later!")
+        }
       }catch(error){
         alert(error.message)
       }
     }
   const onClickComment=()=>{
-    setIsComment(!getIsComment)
-    let date=new Date()
-    date=`${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
-    setDate(date)
-    // console.log(date)
+    if(token){
+      setIsComment(!getIsComment)
+      let date=new Date()
+      date=`${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
+      setDate(date)
+      // console.log(date)
+    }
+    else{
+      alert('Please login to comment!')
+      navigate('/login')
+    }
+    
   }  
   const onClickAddComment=async(id)=>{
     if(!getComment){
       alert('Please add comment')
     }
     else{
-      setIsCommented(true)
-      setCommentContent(getComment)
+      
       setComment('')
       onHandleClickComment(id)
 
@@ -102,7 +121,7 @@ export default function Cards({posts}) {
         </div>
         <div className='card-channel-info'>
             <div><span onClick={()=>onClickLike(posts._id)} className='like-comment'>{isLike?'UnLike':'Like'}</span> {getlikeCount}</div>
-            <div><span onClick={onClickComment} className='like-comment'>Comments:</span> {posts.commentCount}</div>
+            <div><span onClick={onClickComment} className='like-comment'>Comments:</span> {getCommentCount}</div>
         </div>
       </div>
       {
